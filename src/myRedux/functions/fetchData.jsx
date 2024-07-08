cbf6e2e0da8f6ce7b15319cba7348d9b477881bb
Dikshit673 <sachinnavodian@gmail.com>
2024-07-08 07:43:58 +0530

    console api

--- src/myRedux/functions/fetchData.jsx
+++ src/myRedux/functions/fetchData.jsx
@@ -5,7 +5,7 @@ const fetchData = createAsyncThunk("fetchData", async ({ page, pageSize, query, 
     // (query ? `https://newsapi.org/v2/top-headlines?q=${query}&sortBy=popularity&page=${page}&pageSize=${pageSize}&apiKey=${import.meta.env.VITE_NEWS_API_KEY}`
     //     : `https://newsapi.org/v2/top-headlines?country=in&page=${page}&pageSize=${pageSize}&apiKey=${import.meta.env.VITE_NEWS_API_KEY}`);
     // console.log(page, pageSize, query, category);
-    // console.log("apiUrl: ", apiUrl);
+    console.log("apiUrl: ", apiUrl);
     // console.log(!query);
     const data = await fetch(apiUrl);
     const res = await data.json();
