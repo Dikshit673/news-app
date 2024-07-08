b61401cc03b079f68eb0360bb838e67e7ace46c4
Dikshit673 <sachinnavodian@gmail.com>
2024-07-08 09:03:36 +0530

    try catch statement added

--- src/myRedux/functions/fetchData.jsx
+++ src/myRedux/functions/fetchData.jsx
@@ -1,16 +1,20 @@
 import { createAsyncThunk } from "@reduxjs/toolkit"
 import fetchUrl from "./fetchUrl";
 const fetchData = createAsyncThunk("fetchData", async ({ page, pageSize, query, category }) => {
-    const apiUrl = fetchUrl(page, pageSize, query, category);
-    // (query ? `https://newsapi.org/v2/top-headlines?q=${query}&sortBy=popularity&page=${page}&pageSize=${pageSize}&apiKey=${import.meta.env.VITE_NEWS_API_KEY}`
-    //     : `https://newsapi.org/v2/top-headlines?country=in&page=${page}&pageSize=${pageSize}&apiKey=${import.meta.env.VITE_NEWS_API_KEY}`);
-    // console.log(page, pageSize, query, category);
-    console.log("apiUrl: ", apiUrl);
-    // console.log(!query);
-    const data = await fetch(apiUrl);
-    const res = await data.json();
-    // console.log("fetchData", res);
-    return res;
+    try {
+        const apiUrl = fetchUrl(page, pageSize, query, category);
+        // (query ? `https://newsapi.org/v2/top-headlines?q=${query}&sortBy=popularity&page=${page}&pageSize=${pageSize}&apiKey=${import.meta.env.VITE_NEWS_API_KEY}`
+        //     : `https://newsapi.org/v2/top-headlines?country=in&page=${page}&pageSize=${pageSize}&apiKey=${import.meta.env.VITE_NEWS_API_KEY}`);
+        // console.log(page, pageSize, query, category);
+        console.log("apiUrl: ", apiUrl);
+        // console.log(!query);
+        const data = await fetch(apiUrl);
+        const res = await data.json();
+        // console.log("fetchData", res);
+        return res;
+    } catch (error) {
+        return error;
+    }
 })
 
 export default fetchData;
