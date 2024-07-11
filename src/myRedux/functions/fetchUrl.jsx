6c228a1599ef91dbaf97f158a6c292f6d1f6a30b
Dikshit673 <sachinnavodian@gmail.com>
2024-07-11 20:15:58 +0530

    fixes server url

--- src/myRedux/functions/fetchUrl.jsx
+++ src/myRedux/functions/fetchUrl.jsx
@@ -2,15 +2,15 @@ const fetchUrl = (page, pageSize, query, category) => {
     // console.log(page, pageSize, query, category);
     if (query) {
         if (category !== 'general') {
-            return `https://newsapi.org/v2/top-headlines?q=${query}&category=${category}&category=${category}&page=${page}&pageSize=${pageSize}&apiKey=${import.meta.env.VITE_NEWS_API_KEY}`
+            return `https://persist-news-server.vercel.app/news?q=${query}&category=${category}&page=${page}&pageSize=${pageSize}`
         } else {
-            return `https://newsapi.org/v2/top-headlines?q=${query}&page=${page}&pageSize=${pageSize}&apiKey=${import.meta.env.VITE_NEWS_API_KEY}`
+            return `https://persist-news-server.vercel.app/news?q=${query}&page=${page}&pageSize=${pageSize}`
         }
     } else {
         if (category !== 'general') {
-            return `https://newsapi.org/v2/top-headlines?country=in&category=${category}&page=${page}&pageSize=${pageSize}&apiKey=${import.meta.env.VITE_NEWS_API_KEY}`
+            return `https://persist-news-server.vercel.app/news?country=in&category=${category}&page=${page}&pageSize=${pageSize}`
         } else {
-            return `https://newsapi.org/v2/top-headlines?country=in&page=${page}&pageSize=${pageSize}&apiKey=${import.meta.env.VITE_NEWS_API_KEY}`
+            return `https://persist-news-server.vercel.app/news?country=in&page=${page}&pageSize=${pageSize}`
         }
     }
 }
