e54c9b36ea7bd852bb1edf67325016e000b434e7
Dikshit673 <sachinnavodian@gmail.com>
2024-07-11 20:18:54 +0530

    fix fetchdata

--- src/myRedux/functions/fetchData.jsx
+++ src/myRedux/functions/fetchData.jsx
@@ -3,14 +3,12 @@ import fetchUrl from "./fetchUrl";
 const fetchData = createAsyncThunk("fetchData", async ({ page, pageSize, query, category }) => {
     try {
         const apiUrl = fetchUrl(page, pageSize, query, category);
-        // (query ? `https://newsapi.org/v2/top-headlines?q=${query}&sortBy=popularity&page=${page}&pageSize=${pageSize}&apiKey=${import.meta.env.VITE_NEWS_API_KEY}`
-        //     : `https://newsapi.org/v2/top-headlines?country=in&page=${page}&pageSize=${pageSize}&apiKey=${import.meta.env.VITE_NEWS_API_KEY}`);
         // console.log(page, pageSize, query, category);
-        console.log("apiUrl: ", apiUrl);
+        // console.log("apiUrl: ", apiUrl);
         // console.log(!query);
         const data = await fetch(apiUrl);
         const res = await data.json();
-        console.log("fetchData", res);
+        // console.log("fetchData", res);
         return res;
     } catch (error) {
         return error;
